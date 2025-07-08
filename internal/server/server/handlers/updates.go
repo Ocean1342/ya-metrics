849bebc7ff80ba01650152e3e7a1d26541678422
Pashichev Anton <anton.pashichev@lenvendo.ru>
2025-07-08 21:43:14 +0300

    iter12: add updates handler

--- internal/server/server/handlers/updates.go
+++ internal/server/server/handlers/updates.go
@@ -34,16 +34,8 @@ func (h *Handler) Updates(writer http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	tx, err := h.db.Begin()
-	if err != nil {
-		writer.WriteHeader(http.StatusBadRequest)
-		http.Error(writer, err.Error(), http.StatusBadRequest)
-		return
-	}
 	for _, m := range metrics {
 		var mType string
-		/*		var delta sql.NullInt64
-				var value sql.NullFloat64*/
 		var delta int64
 		var value float64
 		switch strings.ToLower(m.MType) {
@@ -62,12 +54,18 @@ func (h *Handler) Updates(writer http.ResponseWriter, req *http.Request) {
 			h.log.Errorf("get undefened type:%s, ID:%s, Delta:%d, Value: %d", m.MType, m.ID, m.Delta, m.Value)
 			continue
 		}
+		tx, err := h.db.Begin()
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			http.Error(writer, err.Error(), http.StatusBadRequest)
+			return
+		}
 		_, err = tx.ExecContext(req.Context(),
 			"INSERT INTO metrics (id, mtype, delta, value) VALUES ($1,$2,$3,$4) "+
 				"ON CONFLICT (id) DO UPDATE SET delta=EXCLUDED.delta, value=EXCLUDED.value",
 			m.ID, mType, delta, value,
 		)
-		fmt.Println(m.ID, mType, delta, value)
+		h.log.Info(m.ID, mType, delta, value)
 		if err != nil {
 			h.log.Errorf("error on transaction. err:%s", err)
 			tx.Rollback()
@@ -75,9 +73,10 @@ func (h *Handler) Updates(writer http.ResponseWriter, req *http.Request) {
 			http.Error(writer, err.Error(), http.StatusBadRequest)
 			return
 		}
+		err = tx.Commit()
+		if err != nil {
+			h.log.Errorf("tx commit err:%s", err)
+		}
 	}
-	err = tx.Commit()
-	if err != nil {
-		h.log.Errorf("tx commit err:%s", err)
-	}
+
 }
