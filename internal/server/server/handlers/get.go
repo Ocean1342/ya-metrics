c85950807d07eeb51354d7809ce62d3d8fd08919
Pashichev Anton <anton.pashichev@lenvendo.ru>
2025-07-23 15:33:20 +0300

    iter14: edits

--- internal/server/server/handlers/get.go
+++ internal/server/server/handlers/get.go
@@ -20,35 +20,40 @@ func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	typeName := chi.URLParam(r, "type")
-
 	if !h.availableMetricsTypes.Isset(typeName) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	name := chi.URLParam(r, "name")
-	g := h.gaugeStorage.Get(name)
-	if g != nil {
-		w.WriteHeader(http.StatusOK)
-		_, err := w.Write([]byte(fmt.Sprintf("%v", g.GetValue())))
+
+	switch typeName {
+	case mdata.GAUGE:
+		g := h.gaugeStorage.Get(name)
+		if g != nil {
+			w.WriteHeader(http.StatusOK)
+			_, err := w.Write([]byte(fmt.Sprintf("%v", g.GetValue())))
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			return
+		}
+	case mdata.COUNTER:
+		c, err := h.countStorage.Get(name)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		return
-	}
-	c, err := h.countStorage.Get(name)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	if c != nil {
-		w.WriteHeader(http.StatusOK)
-		_, err := w.Write([]byte(fmt.Sprintf("%v", c.GetValue())))
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+		if c != nil {
+			w.WriteHeader(http.StatusOK)
+			_, err := w.Write([]byte(fmt.Sprintf("%v", c.GetValue())))
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
 		}
 	}
+
 	w.WriteHeader(http.StatusNotFound)
 	w.Write([]byte(""))
 }
