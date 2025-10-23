c35151ce4e6bcb4d6782879f312ab7037b499f2b
Pashichev Anton <a.pashichev@rambler-co.ru>
2025-10-23 21:05:28 +0300

    iter21: add json file cfg

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -11,6 +11,7 @@ import (
 	"strconv"
 	"time"
 	"ya-metrics/internal/agent/concurrencyagent"
+	"ya-metrics/internal/agent/config"
 	"ya-metrics/pkg/crypto"
 )
 
@@ -30,7 +31,12 @@ func main() {
 	secretKey := flag.String("k", "", "secret key")
 	rateLimit := flag.Int("l", 1, "rate limit")
 	cryptoPublicKey := flag.String("crypto-key", "", "crypto public key")
+	cfgFilePath := flag.String("config", "", "crypto public key")
 	flag.Parse()
+
+	if os.Getenv("CONFIG") != "" {
+		*cfgFilePath = os.Getenv("CONFIG")
+	}
 	if os.Getenv("ADDRESS") != "" {
 		*host = os.Getenv("ADDRESS")
 	}
@@ -62,6 +68,33 @@ func main() {
 	if os.Getenv("CRYPTO_KEY") != "" {
 		*cryptoPublicKey = os.Getenv("CRYPTO_KEY")
 	}
+
+	if *cfgFilePath != "" {
+		cfg, err := config.ParseFromFile(*cfgFilePath)
+		if err != nil {
+			sugar.Errorf("wrong config file path: %s", *cfgFilePath)
+		} else {
+			if *host == "" {
+				*host = cfg.Host
+			}
+			if *reportIntervalSec == 0 {
+				*reportIntervalSec = cfg.ReportIntervalSec
+			}
+			if *pollIntervalSec == 0 {
+				*pollIntervalSec = cfg.PollIntervalSec
+			}
+			if *secretKey == "" {
+				*secretKey = cfg.SecretKey
+			}
+			if *rateLimit == 0 {
+				*rateLimit = cfg.RateLimit
+			}
+			if *cryptoPublicKey == "" {
+				*cryptoPublicKey = cfg.CryptoPublicKey
+			}
+		}
+	}
+
 	srvrAddr := fmt.Sprintf("http://%s", *host)
 	timeToWork := time.Duration(180) * time.Second
 	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(timeToWork))
